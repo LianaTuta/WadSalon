d9ecd59925b77a2527dab809586b63a005f3ecaa
LianaTuta <lianamaria20002@gmail.com>
2024-03-10 16:49:39 +0200

    add login page

---
+++ Salon/Salon/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Salon.Models;
+
+namespace Salon.Controllers
+{
+    public class LoginController : Controller
+    {
+        [BindProperty]
+        public UserLoginModel? userLogin { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserLoginModel u)
+        {
+         
+           return RedirectToAction("Privacy","Home");
+          
+
+            return View(u);
+        }
+       
+    }
+}
---
+++ Salon/Salon/Models/UserLoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Salon.Models
+{
+    public class UserLoginModel
+    {
+        [Required(ErrorMessage = "Please Provide Username", AllowEmptyStrings = false)]
+        public string Username
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessage = "Please provide password", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+    }
+}
