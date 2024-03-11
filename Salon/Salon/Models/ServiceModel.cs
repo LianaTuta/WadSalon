d402c2804f319cc5c96267fbf95a1bd56c76b098
LianaTuta <lianamaria20002@gmail.com>
2024-03-11 23:28:59 +0200

    Merger

---
+++ Salon/Salon/Models/ServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Salon.Models
+{
+    public class ServiceModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int SalonId { get; set; }
+        public DateTime AppoinmentDate { get; set; }
+    }
+}
