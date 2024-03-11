f69a28b4df5938368b7c7c0c5785bde1d9f56ba6
LianaTuta <lianamaria20002@gmail.com>
2024-03-11 23:27:46 +0200

    Add services page

---
+++ Salon/Salon/Controllers/ServicesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Salon.Models;
+
+namespace Salon.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services(int salonId)
+        {
+            var service1 = new ServiceModel()
+            {
+                Id = 1,
+                Name = "Service1",
+                SalonId = 1,
+                Description = "Description"
+            };
+            var service2 = new ServiceModel()
+            {
+                Id = 2,
+                Name = "Service1",
+                SalonId = 2,
+                Description = "Description"
+            };
+            var service3 = new ServiceModel()
+            {
+                Id = 3,
+                Name = "Service1",
+                SalonId = 3,
+                Description = "Description"
+            };
+            var service4 = new ServiceModel()
+            {
+                Id = 3,
+                Name = "Service1",
+                SalonId = 4,
+                Description = "Description"
+            };
+            var serviceList = new List<ServiceModel>() { service1,service2,service3,service4 };
+           
+            ViewBag.Services = serviceList;
+            return View(serviceList);
+        }
+
+        public IActionResult Save(ServiceModel serviceModel)
+        {
+            var service1 = new ServiceModel()
+            {
+                Id = 1,
+                Name = "Service1",
+                SalonId = 1,
+                Description = "Description"
+            };
+            var service2 = new ServiceModel()
+            {
+                Id = 2,
+                Name = "Service1",
+                SalonId = 2,
+                Description = "Description"
+            };
+            var service3 = new ServiceModel()
+            {
+                Id = 3,
+                Name = "Service1",
+                SalonId = 3,
+                Description = "Description"
+            };
+            var service4 = new ServiceModel()
+            {
+                Id = 3,
+                Name = "Service1",
+                SalonId = 4,
+                Description = "Description"
+            };
+            var serviceList = new List<ServiceModel>() { service1, service2, service3, service4 };
+            ViewBag.Services = serviceList;
+            return View(serviceList);
+        }
+    }
+}
