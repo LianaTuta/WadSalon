ad05616c75f5c5f9d282fae83b373a43accc6d23
LianaTuta <lianamaria20002@gmail.com>
2024-03-13 03:09:07 +0200

    Add suuccessful book page

--- Salon/Salon/Controllers/ServicesController.cs
+++ Salon/Salon/Controllers/ServicesController.cs
@@ -41,39 +41,9 @@ namespace Salon.Controllers
             return View(serviceList);
         }
 
-        public IActionResult Save(ServiceModel serviceModel)
+        public IActionResult SuccessfullBook(ServiceModel serviceModel)
         {
-            var service1 = new ServiceModel()
-            {
-                Id = 1,
-                Name = "Service1",
-                SalonId = 1,
-                Description = "Description"
-            };
-            var service2 = new ServiceModel()
-            {
-                Id = 2,
-                Name = "Service1",
-                SalonId = 2,
-                Description = "Description"
-            };
-            var service3 = new ServiceModel()
-            {
-                Id = 3,
-                Name = "Service1",
-                SalonId = 3,
-                Description = "Description"
-            };
-            var service4 = new ServiceModel()
-            {
-                Id = 3,
-                Name = "Service1",
-                SalonId = 4,
-                Description = "Description"
-            };
-            var serviceList = new List<ServiceModel>() { service1, service2, service3, service4 };
-            ViewBag.Services = serviceList;
-            return View(serviceList);
+            return View(serviceModel);
         }
     }
 }
